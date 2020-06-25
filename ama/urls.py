da6dfde2948c32b849dc67507d71c969f49a8be8
akram256 <mukasaakram55@gmail.com>
2020-06-25 23:52:38 +0300

    work on deployment

--- ama/urls.py
+++ ama/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('africamyafrica/', admin.site.urls),
      path('api/v1/', include('api_routes.urls')),
 ]
