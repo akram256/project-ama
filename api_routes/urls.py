ac551d445614951ce2bbcda01538b61a618df931
akram256 <mukasaakram55@gmail.com>
2020-08-05 22:12:07 +0300

    work on errors

--- api_routes/urls.py
+++ api_routes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from authentication.views import LoginAPIView,TestView,AddSubscription,VerifySubscriptionView,VerifyAccount,UserProfileView, RegistrationAPIView,UpdateProfileView,SchoolRegistrationAPIView, AgeCategoryView
+from authentication.views import LoginAPIView,TestView,AddSubscription,VerifySubscriptionView,VerifyAccount,UserProfileView, RegistrationAPIView,UpdateProfileView, AgeCategoryView
 # ,SchoolLoginAPIView
 
 app_name= 'authentication'
@@ -9,7 +9,7 @@ urlpatterns = [
     path('verify/',VerifyAccount.as_view(),name='verify'),
     path("register", RegistrationAPIView.as_view(), name='register'),
     path("login",LoginAPIView.as_view(), name='login'),
-    path("school/register",SchoolRegistrationAPIView.as_view(),name='school-register'),
+    # path("school/register",SchoolRegistrationAPIView.as_view(),name='school-register'),
     path("age/category", AgeCategoryView.as_view(),name='age-category'),
     path("update/profile/<str:id>", UpdateProfileView.as_view(),name='update-profile'),
     path("view/profile/<str:id>", UserProfileView.as_view(),name='view-profile'),
--- authentication/models.py
+++ authentication/models.py
@@ -25,6 +25,7 @@ class User(AbstractBaseUser, PermissionsMixin, BaseAbstractModel):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_new_user=models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name','last_name',]
