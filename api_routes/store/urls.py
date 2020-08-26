051fdbedd18add008d8a2326fc673b33ca009290
akram256 <mukasaakram55@gmail.com>
2020-08-26 03:10:43 +0300

    work on the get one product

--- api_routes/store/urls.py
+++ api_routes/store/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
-from store.views import ProductCategoryView, StoreView
+from store.views import ProductCategoryView, StoreView,ProductRetrieveUpdateView
 
 app_name= 'store'
 urlpatterns = [
      path('product/category',ProductCategoryView.as_view(),name='category'),
      path('products',StoreView.as_view(),name='products'),
+     path('products/<str:id>',ProductRetrieveUpdateView.as_view(),name='products'),
 
 
 ]
